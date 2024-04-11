08a8a903e5ad5c65ea96641c9245282800e7e4aa
marcosdsg <marcos.dsg91@gmail.com>
2024-04-11 10:55:46 -0300

    Adicionando biblioteca de palavras como arquivo txt

--- app/Palavras.hs
+++ app/Palavras.hs
@@ -1,5 +1,7 @@
 module Palavras where
 
+import Data.Char
+
 palavrasList :: [String]
 palavrasList =
   [ "CARRO"
@@ -9,3 +11,12 @@ palavrasList =
   , "COMPUTADOR"
   , "CANETA"
   ]
+
+getWords :: IO [String]
+getWords = do
+    getWords <- readFile "words.txt"
+    let x = words . map toUpper $ removePunc getWords
+    return x
+    
+removePunc :: String -> String
+removePunc xs = [x | x <- xs, x `notElem` "-[],.?!:;\"\'"]
