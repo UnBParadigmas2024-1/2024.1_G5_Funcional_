28c55fbb0ee2dc084a95568fb4f2eb0a8038ce3d
pedrohelias <pedrohelias95@hotmail.com>
2024-04-11 17:09:30 -0300

    funcionando design basico com letras

--- app/modulos/logico.hs
+++ app/modulos/logico.hs
@@ -1,3 +1,51 @@
+countCaract :: String -> IO () --conta o tamanho da palavra
+countCaract [] = putStrLn "Tamanho 0!"
+countCaract str = do
+    let tam = length str
+    putStrLn $ "Tamanho: " ++ show tam
+
+
+
+forLoop :: Int -> IO ()
+forLoop 0 = putStrLn $ "0 iteracoes"
+forLoop it = 
+    if it > 0
+        then do
+            putStrLn $ "iteracao" ++ show it 
+            forLoop (it-1)
+        else 
+            return()
+
+
+temLetra :: String -> Char -> Int -> IO () 
+temLetra [] _ _ = putStrLn $ "Não tem essa letra na palavra" 
+temLetra (h:t) char tam = do 
+    let futuro = 0
+    if h == char
+        then do 
+            putStrLn $ "Tem a letra " ++ show char ++ " na posicao" ++ show futuro 
+            temLetra t char (futuro+1)
+        else 
+            temLetra t char (futuro+1)
+
+
+percorrerString :: String -> Char -> IO ()
+percorrerString str char = percorrerStringAux str char 0
+    where
+        percorrerStringAux :: String -> Char -> Int -> IO ()
+        percorrerStringAux [] _ _ = putStrLn " Fim da string."
+        percorrerStringAux (c:cs) char indice = do
+            if c == char
+                then do  
+                    --putStrLn $ "Caractere atual: " ++ [c] ++ ", Índice: " ++ show indice ++ " - Este está contido"
+                    putStr $ [char]
+                    percorrerStringAux cs char (indice + 1)
+                else do 
+                    --putStrLn $ "Caractere atual: " ++ [c] ++ ", Índice: " ++ show indice
+                    putStr $  " _ "
+                    percorrerStringAux cs char (indice + 1)
+
+
 
 validaEntUser :: Char -> Bool --funcao para validar a entrada do usuario
 validaEntUser a 
@@ -46,9 +94,12 @@ whileLoop a lista =    -- validaEntUser elementoAMenos para chamar o validador
         return ()
 
 
+
+
 main = do --main de teste
 
     let totalVida = 5
     let lista = ['a'..'z']
+    let palavra = "teste"
 
     whileLoop (totalVida :: Int) lista --é chamado assim por ser uma funça~oque imprime na tela, logo função IO. Mas pode ser chamada apenas whileLoop 4
